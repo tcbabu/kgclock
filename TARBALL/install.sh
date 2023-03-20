befd9bd882b0dfc7d624099757a016239b2a8e25
Kulina <kulinasys@gmail.com>
2023-03-20 15:02:37 +0530

    local kulina.h removed

--- TARBALL/install.sh
+++ TARBALL/install.sh
@@ -1,8 +1,8 @@
-install -m 755 -s  kgclock  /usr/bin/kgclock
+install -m 755   kgclock  /usr/bin/kgclock
 mkdir -p /usr/share/kgclock
 install -m 644  kgclock.png /usr/share/kgclock/kgclock.png
 install -m 644  beep.wav /usr/share/kgclock
 install -m 644  bell.wav /usr/share/kgclock
-install -m 644  kgbelal1.wav /usr/share/kgclock
-install -m 644  kgbelal2.wav /usr/share/kgclock
+install -m 644  kgbell1.wav /usr/share/kgclock
+install -m 644  kgbell2.wav /usr/share/kgclock
 install -m 644  kgclock.desktop /usr/share/applications
